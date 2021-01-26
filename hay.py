05b7be12b5521531a43ece1aa2b5264fc9ec90c6
nizar-sys <74032907+nizar-sys@users.noreply.github.com>
2021-01-26 10:03:49 +0700

    Add files via upload

---
+++ hay.py
@@ -0,0 +1,14 @@
+print('pilih menu nya :))')
+
+print('1. Bilang i love u')
+print('2. Gombal')
+
+a = int(input('menu : '))
+
+if a == 1:
+    kali = int(input('mau berapa kali ? '))
+
+    print('love u bbyyyyy:))\n' * kali)
+else:
+    print('km tau ga bedanya km sama program ?')
+    print('kalo program aku yang bikin, kalo kamu dibikin buat aku:) heheehe')
