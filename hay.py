5673655cb8811ada559da5bb58591a0155eab7f6
nizar-sys <74032907+nizar-sys@users.noreply.github.com>
2021-01-26 12:04:57 +0700

    Add files via upload

--- hay.py
+++ hay.py
@@ -1,14 +1,32 @@
-print('pilih menu nya :))')
+print('hai selamat datang, aku adalah bot di program ini!')
+print('pilih menu di bawah ini')
 
-print('1. Bilang i love u')
-print('2. Gombal')
+print('1. Bilang i love u ke kamu')
+print('2. Gombalin kamu')
+print('3. Keluar')
 
-a = int(input('menu : '))
+pilih = int(input('pilihan kamu : '))
 
-if a == 1:
-    kali = int(input('mau berapa kali ? '))
+if pilih == 1:
+    kali = int(input('berapa kali ? '))
+    print('i love uu\n' * kali)
 
-    print('love u bbyyyyy:))\n' * kali)
+    ulang = str(input('ulang ? (y/n) '))
+    while (ulang == 'y'):
+        kali = int(input('berapa kali ? '))
+        print('i love uu\n' * kali)
+        break
+elif pilih == 2:
+    print('kamu tau ga apa bedanya kamu sama aku ?')
+    print('yaa. kalo aku dibuat sama program, kalo kamu itu buat akuu tee-hee:P')
+
+    lagi = str(input('mau yang lain ? (y/n) '))
+    while (lagi == 'y'):
+        print('cape cape overthinking buat kamu')
+        print('eh ternyata kamu nya juga suka sama aku')
+        break
 else:
-    print('km tau ga bedanya km sama program ?')
-    print('kalo program aku yang bikin, kalo kamu dibikin buat aku:) heheehe')
+    yakin = str(input('yakin ? (y/n) '))
+    while(yakin == 'y'):
+        print('byee, makasih udah main sama aku:)')
+        break
