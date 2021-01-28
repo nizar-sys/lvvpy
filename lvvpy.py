d50447fed700fd82ef5d0eb9ab0757fa8c5c1104
nizar-sys <74032907+nizar-sys@users.noreply.github.com>
2021-01-28 22:04:35 +0700

    Add files via upload

--- lvvpy.py
+++ lvvpy.py
@@ -1,36 +1,89 @@
+import time
+
+
 def ily():
+    kata = "i love uu bbyy\n"
     inputIly = int(input('berapa kali ? '))
-    print('i love uu\n' * inputIly)
+
+    for inputIly in range(inputIly, 0, -1):
+        time.sleep(0.1)
+        print(kata)
 
     ulang = str(input('lagi ? (y/n) '))
     while (ulang == 'y'):
         ily()
     if ulang == 'n':
+        time.sleep(0.5)
         menu()
 
 
 def gombal():
-    print('kamu tau ga bedanya kamu sama aku ?')
-    print('kalo aku dibuat sama program, kalo kamu dibuat untuk aku... tee-hee')
-    kembali = str(input('kembali ke menu ? (y) '))
+
+    print("aku mau ngomong sesuatu, tapi malu... jadi disini aja ya hehe")
+    time.sleep(0.5)
+    nama = str(input("masukin nama kamu yaa : "))
+    print("sekarang jawab jujur ya!")
+    pacar = str(
+        input("sebenernya aku suka sama kamu, kamu mau jadi pacar aku ga ? (y/n) "))
+    if pacar == 'y':
+        print(nama + " kamu sekarang jadi pacar aku yaaa\n")
+        time.sleep(0.5)
+        print("oh ya, aku ada pantun buat kamu")
+        time.sleep(0.5)
+        print("ikan hiu makan tomat")
+        time.sleep(0.5)
+        print("ai lop yuuu hehehhe")
+    else:
+        print("kenapa ?:(")
+        time.sleep(0.5)
+        print("\nyaudah deh, btw aku ada pantun buat kamu")
+        time.sleep(0.5)
+        print("ikan hiu makan tomat")
+        time.sleep(0.5)
+        print("ai lop yuuu hehehhe")
+    kembali = str(input('kembali ke menu (y/n) '))
     if kembali == 'y':
+        menu()
+    else:
+        print("udah sana, aku malu>_<")
+        time.sleep(0.5)
         menu()
 
 
+def about():
+    print("==========================About Me======================================")
+    print("| Name : Muhamad Nizar                                                 | ")
+    print("| Github : github.com/nizar-sys | Instagram : instagram.com/@niizar.id | ")
+    print("========================================================================")
+    kembali = str(input('kembali ke menu (y/n) '))
+    if kembali == 'y':
+        menu()
+    else:
+        time.sleep(0.5)
+        about()
+
+
 def menu():
-    print('===Menu===')
-    print('1. Bilang i love u')
-    print('2. Gombal')
-    print('3. Keluar')
+    print('==============Menu================')
+    print('| 1. Bilang i love u  3. About   |')
+    print('| 2. Gombal           4. Keluar  |')
+    print('==================================')
 
     menu = str(input('Pilih menu : '))
 
     if menu == '1':
+        time.sleep(0.5)
         ily()
     elif menu == '2':
+        time.sleep(0.5)
         gombal()
+    elif menu == '3':
+        time.sleep(0.5)
+        about()
+    elif menu == '4':
+        exit("byee, makasih ya udah ngunjungin akuu")
     else:
-        exit('byee, makasi ya udah ngunjungin akuuu')
+        print("menu yang dipilih tidak ada di list!")
 
 
 if __name__ == "__main__":
